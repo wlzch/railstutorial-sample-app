002d509b4094347a9ebcee0cc8081f4ab2917f03
wlzch <wilzichi92@gmail.com>
2012-10-24 15:33:37 +0700

    Add static pages.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector 'h1', :text => 'Sample App'
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector 'title', :text => ' | Home'
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector 'h1', :text => 'Help'
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector 'title', :text => ' | Help'
+    end
+  end
+
+  describe "About Page" do
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector 'h1', :text => 'About'
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector 'title', :text => ' | About'
+    end
+  end
+end
