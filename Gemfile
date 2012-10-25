4058552493d3db61fb6472a8c75fee1bd9a4bcb9
wlzch <wilzichi92@gmail.com>
2012-10-25 13:47:01 +0700

    Use postgres, spork, guard.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,16 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'pg', '0.12.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
-    gem 'sqlite3', '1.3.5'
     gem 'rspec-rails', '2.11.0'
+    gem 'guard-rspec', '1.2.1'
+    gem 'guard-spork', '1.2.0'
+    gem 'spork', '0.9.2'
 end
 
 
@@ -16,10 +19,6 @@ end
 group :assets do
   gem 'sass-rails',   '3.2.5'
   gem 'coffee-rails', '3.2.2'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '1.2.3'
 end
 
@@ -27,8 +26,5 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
     gem 'capybara', '1.1.2'
-end
-
-group :production do
-    gem 'pg', '0.12.2'
+    gem 'rb-inotify', '0.8.8'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,14 @@ describe "StaticPages" do
       page.should have_selector 'h1', :text => 'Sample App'
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector 'title', :text => ' | Home'
+      page.should have_selector 'title', :text => 'Ruby on Rails Tutorial Sample App'
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector 'title', :text => '| Home'
     end
   end
 
@@ -34,6 +39,18 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector 'title', :text => ' | About'
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'h1', :text => 'Contact'
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector 'title', :text => ' | Contact'
     end
   end
 end
